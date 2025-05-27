bcbd462049b77f70a0ea95513f8534fe1571b649
ivaniln <ivaniln427@gmail.com>
2025-05-27 23:58:43 +0300

    playing with inputs

--- main.py
+++ main.py
@@ -10,10 +10,24 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 layer1 = Layer_Dense(4, 8)
-layer2 = Layer_Dense(8, 2)
+layer2 = Layer_Dense(8, 16)
+layer3 = Layer_Dense(16, 8)
+layer4 = Layer_Dense(8, 4)
 
-layer1.forward(inputs)
-layer2.forward(layer1.output)
+while True:
+    
+    n = int(input())
+    inputs = []
+    for i in range(n):
+        a = int(input())
+        inputs.append(a)
 
-print(layer1.output)
-print(layer2.output)
+    layer1.forward(inputs)
+    layer2.forward(layer1.output)
+    layer3.forward(layer2.output)
+    layer4.forward(layer3.output)
+
+    print(layer1.output)
+    print(layer2.output)
+    print(layer3.output)
+    print(layer4.output)
